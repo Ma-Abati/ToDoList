910ff1b7969448003545b6c95bfb30d204ae3af0
Maia Fattori Abati <maiaabati@gmail.com>
2025-07-14 11:00:15 -0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Maia Fattori Abati on 14/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
